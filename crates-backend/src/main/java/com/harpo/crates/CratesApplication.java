4575e2a0c667178e279e68e7c9a66eec85125321
nomander <nomander17@gmail.com>
2025-08-09 18:18:50 +0530

    Add backend bootstrap

---
+++ crates-backend/src/main/java/com/harpo/crates/CratesApplication.java
@@ -0,0 +1,13 @@
+package com.harpo.crates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CratesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CratesApplication.class, args);
+	}
+
+}
---
+++ crates-backend/src/test/java/com/harpo/crates/CratesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harpo.crates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CratesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
