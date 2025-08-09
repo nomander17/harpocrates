79b3307ca124cd453b245c41c8354124a488ef71
nomander <nomander17@gmail.com>
2025-08-09 21:43:30 +0530

    argon2

---
+++ crates-frontend/src/config.js
@@ -0,0 +1,3 @@
+import.meta.env;
+
+export const API_URL = import.meta.env.VITE_BACKEND_API_URL;
--- crates-frontend/src/pages/Home.jsx
+++ crates-frontend/src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import "./Home.css";
 import { useState } from "react";
-import { toast } from 'sonner';
+import { toast } from "sonner";
+import { API_URL } from "../config";
+import argon2 from "argon2-browser/dist/argon2-bundled.min.js";
 
 export default function Home() {
   const [form, setForm] = useState({
@@ -38,7 +40,42 @@ export default function Home() {
       return;
     }
     console.log("Login pressed:", form.username, form.password);
+    attemptLogin(form.password);
   };
+
+  async function attemptLogin(password) {
+    // fetch user salt
+    // gen hash
+    // send hash + user and attempt to establish session
+    try {
+      const res = await fetch(`${API_URL}/api/auth/salts?email=user`);
+      const { auth_salt, encryption_salt } = await res.json();
+
+      const userAuthHash = generateAuthenticationHash(password, auth_salt);
+      console.log(userAuthHash);
+    } catch (err) {
+      console.error(err);
+    }
+
+    // todo rest
+  }
+
+  async function generateAuthenticationHash(password, auth_salt) {
+    const hash = await argon2.hash({
+      pass: password,
+      salt: auth_salt,
+      type: argon2.ArgonType.Argon2id,
+      time: 3,
+      mem: 4096,
+      hashLen: 32,
+      parallelism: 1,
+    });
+
+    console.log("Hash hex:", hash.hashHex);
+    console.log("Encoded:", hash.encoded);
+
+    return hash.hashHex;
+  }
 
   const switchForm = (e, type) => {
     e.preventDefault();
